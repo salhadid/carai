70539aef7f73685f5fd03b793af699fb036c342d
smalhadid <smalhadid@gmail.com>
2023-04-24 15:58:00 -0400

    created models and poller.py

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
 
-# Create your views here.
+
+# class
