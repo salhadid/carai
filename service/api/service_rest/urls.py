a9109a60fac4421349492a7209b490fa654ac934
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-25 15:09:28 -0400

    added appointment endpoints

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,9 +1,28 @@
 from django.urls import path
 
 # from .views import
-from .views import api_technician, api_technicians
+from .views import (
+    api_technician,
+    api_technicians,
+    api_appointment,
+    api_appointments,
+    api_cancel_appointment,
+    api_finish_appointment,
+)
 
 urlpatterns = [
     path("technicians/", api_technicians, name="api_technicians"),
     path("technicians/<int:pk>/", api_technician, name="api_technician"),
+    path("appointments/", api_appointments, name="api_appointments"),
+    path("appointments/<int:pk>/", api_appointment, name="api_appointment"),
+    path(
+        "appointments/<int:pk>/cancel/",
+        api_cancel_appointment,
+        name="api_cancel_appointment",
+    ),
+    path(
+        "presentations/<int:pk>/finish/",
+        api_finish_appointment,
+        name="api_finish_appointment",
+    ),
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 import json
@@ -53,7 +53,8 @@ def api_technicians(request):
             encoder=TechnicianEncoder,
         )
     else:
-        # TODO: consider adding logic here to check/handle employee_id
+        # TODO: add logic here to check/handle employee_id
+        # need a try/except for an IntegrityError
         content = json.loads(request.body)
         technician = Technician.objects.create(**content)
         return JsonResponse(
@@ -96,3 +97,109 @@ def api_technician(request, pk):
 
 
 # appointment views
+@require_http_methods(["GET", "POST"])
+def api_appointments(request):
+    """
+    API endpoint to get a list of Appointments or create a single Appointment.
+
+    GET:
+    Returns a dictionary with a single key "appointments" which is a list of Appointment
+    resources.
+
+    {
+        "appointments": [
+            {
+                "id": (str) unique id of the resource in the database
+                "date_time": (datetime) Appointment date and time,
+                "reason": (str) Reason for the appointment (i.e. "oil change"),
+                "status": (str) Status of the appointment (CREATED, CANCELED, FINISHED),
+                "vin": (str) Vehicle's VIN Number,
+                "customer": (str) Customer's first and last name,
+                "technician": (str) Technician assigned to service the vehicle
+            },
+            ...
+        ]
+    }
+
+    POST:
+    Creates an appointment resource and returns its details.
+    {
+        "date_time": (datetime) Appointment date and time,
+        "reason": (str) Reason for the appointment (i.e. "oil change"),
+        "status": (str) Status of the appointment (CREATED, CANCELED, FINISHED),
+        "vin": (str) Vehicle's VIN Number,
+        "customer": (str) Customer's first and last name,
+        "technician": (str) Technician assigned to service the vehicle
+    }
+    """
+    if request.method == "GET":
+        appointments = Appointment.objects.all()
+        return JsonResponse(
+            {"appointments": appointments},
+            encoder=AppointmentEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        appointment = Appointment.objects.create(**content)
+        return JsonResponse(
+            appointment,
+            encoder=AppointmentEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["GET", "DELETE"])
+def api_appointment(request, pk):
+    """
+    Returns the details for a single Appointment specified by the pk parameter.
+    Can also delete the specified Appointment.
+
+    Returns a dictionary with a single key "appointment" with the specific Appointment's
+    information.
+
+    GET:
+    {
+        "appointment": {
+                "id": primary key (pk),
+                "date_time": (datetime) Appointment date and time,
+                "reason": (str) Reason for the appointment (i.e. "oil change"),
+                "status": (str) Status of the appointment (CREATED, CANCELED, FINISHED),
+                "vin": (str) Vehicle's VIN Number,
+                "customer": (str) Customer's first and last name,
+                "technician": (str) Technician assigned to service the vehicle
+            }
+    }
+    """
+    if request.method == "DELETE":
+        appointment = get_object_or_404(Appointment, id=pk)
+        appointment.delete()
+        message = f"Deleted appointment: {appointment.id} for VIN: {appointment.vin}"
+        return JsonResponse({"message": message})
+    else:
+        # GET response for individual technician id
+        appointment = get_object_or_404(Appointment, id=pk)
+        return JsonResponse(
+            {"appointment": appointment}, encoder=AppointmentEncoder, safe=False
+        )
+
+
+@require_http_methods(["PUT"])
+def api_cancel_appointment(request, pk):
+    appointment = get_object_or_404(Appointment, id=pk)
+    appointment.cancel()
+    return JsonResponse(
+        appointment,
+        encoder=AppointmentEncoder,
+        safe=False,
+    )
+
+
+@require_http_methods(["PUT"])
+def api_finish_appointment(request, pk):
+    appointment = get_object_or_404(Appointment, id=pk)
+    appointment.finish()
+    return JsonResponse(
+        appointment,
+        encoder=AppointmentEncoder,
+        safe=False,
+    )
