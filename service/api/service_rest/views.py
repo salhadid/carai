b013b91b6c411107e5eea31ece4d69a495f21a8d
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-25 11:12:46 -0400

    added imports

--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+
+from .encoders import (
+    AutomobileVOEncoder,
+    CustomerVOEncoder,
+    TechnicianEncoder,
+    StatusEncoder,
+    AppointmentEncoder,
+)
+
+from .models import CustomerVO, AutomobileVO, Technician, Status, Appointment
 
 # Create your views here.
