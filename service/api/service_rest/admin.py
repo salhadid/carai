f47e4ceccb9dfe709d4d081f4edee54c3c4e4340
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-25 18:15:10 -0400

    registered models with admin

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Technician, Status, Appointment, AutomobileVO
+
+
+@admin.register(Technician)
+class TechnicianAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(AutomobileVO)
+class AutomobileVOAdmin(admin.ModelAdmin):
+    pass
