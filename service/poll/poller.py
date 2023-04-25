f9b1abb4a244be79c54ff823894b6533d8a147d1
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-25 13:17:39 -0400

    added get_automobiles to poller

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -11,15 +11,33 @@ django.setup()
 
 # Import models from service_rest, here.
 # from service_rest.models import Something
+from service_rest.models import AutomobileVO
 
-def poll():
+
+def get_automobiles():
+    response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles/")
+    content = json.loads(response.content)
+    for automobile in content["autos"]:
+        AutomobileVO.objects.update_or_create(
+            vin=automobile["vin"],
+            defaults={
+                "vin": automobile["vin"],
+            },
+        )
+
+
+def poll(repeat=True):
     while True:
-        print('Service poller polling for data')
+        print("Service poller polling for data")
         try:
             # Write your polling logic, here
-            pass
+            get_automobiles()
         except Exception as e:
             print(e, file=sys.stderr)
+
+        if not repeat:
+            break
+
         time.sleep(60)
 
 
