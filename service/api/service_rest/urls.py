067ac7dbe2b82ec886fdf73645b912cb8de8f4a5
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-25 14:15:38 -0400

    added urlpatterns with paths to technician endpoints

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
 # from .views import
+from .views import api_technician, api_technicians
+
+urlpatterns = [
+    path("technicians/", api_technicians, name="api_technicians"),
+    path("technicians/<int:pk>/", api_technician, name="api_technician"),
+]
