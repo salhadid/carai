e8da28f774ef23266500571e77ef401f31044278
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-26 07:57:26 -0400

    added str representation for Appointment model

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -108,6 +108,9 @@ class Appointment(models.Model):
     def get_api_url(self):
         return reverse("api_show_appointment", kwargs={"pk": self.pk})
 
+    def __str__(self):
+        return f"{self.vin}-{self.appt_date}-{self.appt_time}"
+
     class Meta:
         ordering = (
             "appt_date",
