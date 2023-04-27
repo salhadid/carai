5ad05a29dfccb2530ebe94ac42e86e79d64d2ac0
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-27 17:47:31 -0400

    fixed indenation

--- ghi/app/src/ManufacturersList.js
+++ ghi/app/src/ManufacturersList.js
@@ -2,46 +2,46 @@ import {useState, useEffect} from 'react';
 
 function ManufacturersList() {
 
-    // initialize variables and state 
-    const [manufacturers, setManufacturers] = useState([]);
+  // initialize variables and state 
+  const [manufacturers, setManufacturers] = useState([]);
 
-    // GET the list of manufacturers from the Inventory API
-    async function loadManufacturers() {
-        const response = await fetch('http://localhost:8100/api/manufacturers/');
-        
-        if (response.ok) {
-            const data = await response.json();
-            setManufacturers(data.manufacturers);
-        } else {
-        console.error(response);
-        }
-    };
+  // GET the list of manufacturers from the Inventory API
+  async function loadManufacturers() {
+    const response = await fetch('http://localhost:8100/api/manufacturers/');
 
-    useEffect(() => {
-      loadManufacturers();
-    }, []);
-    
-    return (
-        <div>
-            <h1>Manufacturers</h1>
-            <table className="table table-striped">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                </tr>
-                </thead>
-                <tbody>
-                {manufacturers.map(manufacturer => {
-                    return (
-                    <tr key={manufacturer.id}>
-                        <td>{ manufacturer.name }</td>
-                    </tr>
-                    );
-                })}
-                </tbody>
-            </table>
-        </div>
-    );
-  }
-  
-  export default ManufacturersList;
+    if (response.ok) {
+      const data = await response.json();
+      setManufacturers(data.manufacturers);
+    } else {
+      console.error(response);
+    }
+  };
+
+  useEffect(() => {
+    loadManufacturers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Manufacturers</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {manufacturers.map(manufacturer => {
+            return (
+              <tr key={manufacturer.id}>
+                <td>{ manufacturer.name }</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ManufacturersList;
