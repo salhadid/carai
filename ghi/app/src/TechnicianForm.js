d98f89112668e56176ee8938176decadc3177446
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-27 16:11:11 -0400

    reformatted to 2 spaces

--- ghi/app/src/TechnicianForm.js
+++ ghi/app/src/TechnicianForm.js
@@ -2,83 +2,81 @@ import React, { useState } from 'react';
 
 function TechnicianForm () {
 
-    // initialize variables with state
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [employeeId, setEmployeeId] = useState('');
+  // initialize variables with state
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [employeeId, setEmployeeId] = useState('');
 
-    // event handlers when changes are made to the form
-    const handleFirstNameChange = (event) => {
-        const value = event.target.value;
-        setFirstName(value);
-    }
+  // event handlers when changes are made to the form inputs
+  const handleFirstNameChange = (event) => {
+      const value = event.target.value;
+      setFirstName(value);
+  }
 
-    const handleLastNameChange = (event) => {
-        const value = event.target.value;
-        setLastName(value);
-    }
+  const handleLastNameChange = (event) => {
+      const value = event.target.value;
+      setLastName(value);
+  }
 
-    const handleEmployeeIdChange = (event) => {
-        const value = event.target.value;
-        setEmployeeId(value);
-    }
+  const handleEmployeeIdChange = (event) => {
+      const value = event.target.value;
+      setEmployeeId(value);
+  }
 
-    // handle the data once submitted 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = {};
+  // handle the data once submitted 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = {};
 
-        data.first_name = firstName;
-        data.last_name = lastName;
-        data.employee_id = employeeId;
+    data.first_name = firstName;
+    data.last_name = lastName;
+    data.employee_id = employeeId;
 
 
-        const serviceUrl = 'http://localhost:8080/api/technicians/';
-        const fetchConfig = {
-        method: "post",
-        body: JSON.stringify(data),
-        headers: {
-            'Content-Type': 'application/json',
-            }
-        };
-
-        const response = await fetch(serviceUrl, fetchConfig);
-
-        if (response.ok) {
-        const newTechnician = await response.json();
-        console.log(newTechnician);
-
-        setFirstName('');
-        setLastName('');
-        setEmployeeId('');
-        };
+    const serviceUrl = 'http://localhost:8080/api/technicians/';
+    const fetchConfig = {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: {
+        'Content-Type': 'application/json',
+        }
     };
 
+    const response = await fetch(serviceUrl, fetchConfig);
 
-    return (
-        <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Add a Technician</h1>
-            <form onSubmit={handleSubmit} id="create-technician-form">
-              <div className="form-floating mb-3">
-                <input onChange={handleFirstNameChange} value={firstName} placeholder="First name..." required type="text" name="first_name" id="first_name" className="form-control"/>
-                <label htmlFor="first_name">First Name</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input onChange={handleLastNameChange} value={lastName} placeholder="Last Name..." required type="text" name="last_name" id="last_name" className="form-control"/>
-                <label htmlFor="last_name">Last Name</label>
-              </div>
-              <div className="form-floating mb-3">
-                <input onChange={handleEmployeeIdChange} value={employeeId} placeholder="Employee ID..." type="text" name="employee_id" id="employee_id" className="form-control"/>
-                <label htmlFor="employee_id">Employee ID</label>
-              </div>
-              <button className="btn btn-primary">Create</button>
-            </form>
-          </div>
+    if (response.ok) {
+
+      setFirstName('');
+      setLastName('');
+      setEmployeeId('');
+      };
+  };
+
+
+  return (
+    <div className="row">
+      <div className="offset-3 col-6">
+        <div className="shadow p-4 mt-4">
+          <h1>Add a Technician</h1>
+          <form onSubmit={handleSubmit} id="create-technician-form">
+            <div className="form-floating mb-3">
+              <input onChange={handleFirstNameChange} value={firstName} placeholder="First name..." required type="text" name="first_name" id="first_name" className="form-control"/>
+              <label htmlFor="first_name">First Name</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input onChange={handleLastNameChange} value={lastName} placeholder="Last Name..." required type="text" name="last_name" id="last_name" className="form-control"/>
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input onChange={handleEmployeeIdChange} value={employeeId} placeholder="Employee ID..." type="text" name="employee_id" id="employee_id" className="form-control"/>
+              <label htmlFor="employee_id">Employee ID</label>
+            </div>
+            <button className="btn btn-primary">Create</button>
+          </form>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default TechnicianForm;
