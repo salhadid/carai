a41ad44707d8d8af161053da9c9f9e0f62805bc7
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-27 17:53:00 -0400

    removed comments

--- service/poll/poller.py
+++ service/poll/poller.py
@@ -9,8 +9,6 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "service_project.settings")
 django.setup()
 
-# Import models from service_rest, here.
-# from service_rest.models import Something
 from service_rest.models import AutomobileVO
 
 
