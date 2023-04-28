edda55f548761864245cd5f7043fa9c2d6d96b18
Ryan Curry <16964784+ryancur@users.noreply.github.com>
2023-04-28 17:06:54 -0400

    fix: customer last name

--- ghi/app/src/SalespersonHistory.js
+++ ghi/app/src/SalespersonHistory.js
@@ -71,7 +71,7 @@ function SalespersonHistory() {
                         return (
                             <tr key={sale.id}>
                                 <td>{sale.salesperson.first_name} {sale.salesperson.last_name}</td>
-                                <td>{sale.customer.first_name} {sale.customer.first_name}</td>
+                                <td>{sale.customer.first_name} {sale.customer.last_name}</td>
                                 <td>{sale.automobile}</td>
                                 <td>${sale.price}</td>
                             </tr>
